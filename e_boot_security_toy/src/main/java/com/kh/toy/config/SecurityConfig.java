afa77a0caf5224db3e25ccc3c06856d54efc62ef
azimemory <azimemory@gmail.com>
2021-04-16 07:16:53 +0900

    remember-me finish

--- e_boot_security_toy/src/main/java/com/kh/toy/config/SecurityConfig.java
+++ e_boot_security_toy/src/main/java/com/kh/toy/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
         		.logoutUrl("/member/logout")
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/index");
      
         http.csrf()
         	.ignoringAntMatchers("/mail/**");
