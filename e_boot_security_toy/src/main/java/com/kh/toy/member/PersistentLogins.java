315bd2ceb59091e383ba5313db9524811d092dee
azimemory <azimemory@gmail.com>
2021-04-19 03:32:26 +0900

    자동로그인 설명

--- e_boot_security_toy/src/main/java/com/kh/toy/member/PersistentLogins.java
+++ e_boot_security_toy/src/main/java/com/kh/toy/member/PersistentLogins.java
@@ -14,10 +14,15 @@ public class PersistentLogins {
 
     @Column(nullable = false, length = 64)
     private String username;
-
+    
+    //매 요청마다 새로운 토큰을 만들어서 발급
+    //remember-me 때 헤더에 쿠키가 있다면 그 쿠키의 series로  DB를 조회한다.
+    //series가 같지만 token이 다르다면 자동로그인을 해제한다.
     @Column(nullable = false, length = 64)
     private String token;
-
+    
+    //마지막 사용으로부터 2주까지가 유효기간. 
+    //2주동안 last_used가 사용되지 않으면 자동로그인이 해제된다.
     @Column(name = "last_used",nullable = false,length = 64)
     private LocalDateTime lastUsed;
 }
